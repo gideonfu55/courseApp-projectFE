cf7c463dff1b34d3de9c2ce723621a8dc2fb8413
jcmonkey87 <fuzhonglong@gmail.com>
2022-11-25 15:05:36 +0800

    add login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Login from './components/Login';
 import Register from './components/Register';
 
 function App() {
   return (
     <div className="App">
       <Register />
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Login
+      <section className='email'>
+      </section>
+      <section className='Password'></section>
+    </div>
+  )
+
+}
+
+export default Login
