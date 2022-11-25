5aa67b409f5e87905bee36d2acc0236d8cc524ac
terencebc92 <terencebc92@gmail.com>
2022-11-25 14:20:10 +0800

    Additional setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Register from './components/Register';
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <Register />
     </div>
   );
 }
