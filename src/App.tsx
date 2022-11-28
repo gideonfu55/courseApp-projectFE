0ec34b70d9de2bb614e403afa4c757b796a8b427
terencebc92 <terencebc92@gmail.com>
2022-11-28 11:44:09 +0800

    Register page FE

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import './App.css';
-import Login from './components/Login';
-import Register from './components/Register';
-import { Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
+import React from 'react'
+import './App.css'
+import Login from './components/Login'
+import Register from './components/Register'
+import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      {/* Place any new pages as a Route element inside the Routes tag
+    return (
+        <div className="App">
+            <NavBar />
+            {/* Place any new pages as a Route element inside the Routes tag
       Optional: update NavBar element to expose the routes*/}
-      <Routes>
-        <Route path='/register' element ={<Register />}/>
-        <Route path='/login' element ={<Login />}/>
-      </Routes>
-    </div>
-  );
+            <Routes>
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </div>
+    )
 }
 
-export default App;
+export default App
