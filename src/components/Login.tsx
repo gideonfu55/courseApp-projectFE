64955c44ec104b63fe436740a39616b9fd7181fd
jcmonkey87 <fuzhonglong@gmail.com>
2022-11-28 19:24:49 +0800

    start on email validation feature

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Login = () => {
-  return (
+const Login: React.FC = () => {
+  const [usernameOrEmail, checkEmail] = useState('')
+  const [password, checkPassword] = useState('')
+
+  const [currentErrors, setCurrentErrors] = useState([])
+
+  // Handling the email check
+  const handleEmailCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    checkEmail(e.target.value)
+  }
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+    let formData = {
+      usernameOrEmail: usernameOrEmail,
+      password: password
+    }
+  }
+
+    return (
     <div className="center">
       <form>
         <header className="accountLogin"><h1>Account Login</h1></header>
@@ -10,34 +28,31 @@ const Login = () => {
           <input typeof="text" required></input>
         </section>
 
-        <section className="Password">
+        <section className="password">
           <h5>Password</h5>
           <input typeof="text" required></input>
         </section>
 
         <section className="loginButton">
-          <button id="loginBtn">Login</button>
+          <div className='center'>
+            <button id="loginBtn">Login</button>
+          </div>
         </section>
 
         <div className='center'>
           <section className="resetPW">
-            <h5>Forgot your password?<button id="resetBtn">Reset</button></h5>
+            <h6>Forgot your password?<button id="resetBtn">Reset</button></h6>
           </section>
         </div>
 
         <section className="createAccount">
-          <h5>Create new account</h5>
-          <p>
-            <a href="register">Student</a>
-          </p>
-          <p>
-            <a href="register">Course Creator</a>
-          </p>
+          <h6>Create new account</h6>
+          <p><a href="register">Student</a></p>
+          <p><a href="register">Course Creator</a></p>
         </section>
       </form>
     </div>
   )
-
 }
 
 export default Login
