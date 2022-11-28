2cd6cbb9abdacd71c6a4536a680e68f58f970fd4
jcmonkey87 <fuzhonglong@gmail.com>
2022-11-28 11:47:27 +0800

    login elements update

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,12 +2,41 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login
-      <section className='email'>
-      </section>
-      <section className='Password'></section>
+    <div>
+      <h1 className="">Account Login</h1>
+
+      <form>
+        <section className="email">
+          <p>Email</p>
+          <input typeof="text" required></input>
+        </section>
+
+        <section className="Password">
+          <p>Password</p>
+          <input typeof="text" required></input>
+        </section>
+
+        <section className="loginButton">
+          <button>Login</button>
+        </section>
+
+        <section className="resetPW">
+          <p>Forgot your password?</p>
+          <button>Reset</button>
+        </section>
+
+        <section className="createAccount">
+          <h3>Create new account</h3>
+          <p>
+            <a href="">Student</a>
+          </p>
+          <p>
+            <a href="">Course Creator</a>
+          </p>
+        </section>
+      </form>
     </div>
-  )
+  );
 
 }
 
