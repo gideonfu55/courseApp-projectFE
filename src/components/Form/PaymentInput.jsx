97bed544a7c929f32d71b5c5367e4fdf90838041
jcmonkey87 <fuzhonglong@gmail.com>
2022-12-09 19:54:23 +0800

    add payment methods as tab components

--- src/components/Form/PaymentInput.jsx
+++ src/components/Form/PaymentInput.jsx
@@ -1,31 +1,35 @@
-import { usePaymentInputs, PaymentInputsContainer } from 'react-payment-inputs';
+import { usePaymentInputs } from 'react-payment-inputs';
 import images from 'react-payment-inputs/images'
 
 const PaymentInput = () => {
   const {meta, getCardImageProps, getCardNumberProps, getExpiryDateProps, getCVCProps} = usePaymentInputs();
 
   return (
-    <div className='CCInputBox'>
-      <div className="input-group">
-        <svg {...getCardImageProps({ images })} />
-        <label>Card Number</label>
-        <input {...getCardNumberProps()} />
-      </div>
+    <section id="creditCardDetails">
+      <div className='creditTab-container'>
+        <div className='CCInputBox'>
+          <div className="input-group">
+            <svg {...getCardImageProps({ images })} />
+            <label>Card Number</label>
+            <input {...getCardNumberProps()} />
+          </div>
 
-      <div className="multi-input">
-        <div className="input-group">
-          <label>Valid Till</label>
-          <input {...getExpiryDateProps()} />
-        </div>
+          <div className="multi-input">
+            <div className="input-group">
+              <label>Valid Till</label>
+              <input {...getExpiryDateProps()} />
+            </div>
 
-        <div className="input-group">
-          <label>CVC</label>
-          <input {...getCVCProps()} />
+            <div className="input-group">
+              <label>CVC</label>
+              <input {...getCVCProps()} />
+            </div>
+          </div>
+
+          <small>{meta.isTouched && meta.error && <span>Error: {meta.error}</span>}</small>
         </div>
       </div>
-      
-      <small>{meta.isTouched && meta.error && <span>Error: {meta.error}</span>}</small>
-    </div>
+    </section>
   );
 }
 
