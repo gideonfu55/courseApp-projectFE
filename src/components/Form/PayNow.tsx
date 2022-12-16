e324189b5a37a40e92e9fa9db573fb712c7f311b
jcmonkey87 <fuzhonglong@gmail.com>
2022-12-16 16:03:14 +0800

    additional form parameters for json

--- src/components/Form/PayNow.tsx
+++ src/components/Form/PayNow.tsx
@@ -2,10 +2,12 @@ const PayNow = () => {
   return (
     <section id="payNowDetails" className="tab-content" data-theme="payNow">
       <div className="paymentInputContainer">
-        <h5>PayNow Number to Transfer</h5>
-        <h5>QR Code for Direct PayNow</h5>
-        <input type="checkbox" id="payNowConfirm" name="payNowConfirm" value="" required/>
-        <label htmlFor="payNowConfirm">I confirm that the PayNow will be made within 1hr from this purchase. I acknowledge that the order will be cancelled otherwise.</label>
+        <div className="align-center">
+          <h5>PayNow Number to Transfer</h5>
+          <h5>QR Code for Direct PayNow</h5>
+          <input type="checkbox" id="payNowConfirm" name="payNowConfirm" value="" required/>
+          <label htmlFor="payNowConfirm">I confirm that the PayNow will be made within 1hr from this purchase. I acknowledge that the order will be cancelled otherwise.</label>
+        </div>
       </div>
     </section>
   )
